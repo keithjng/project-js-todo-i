cc498db99a01836be89ed4841270f51a87871a65
Keith Ng <keith.jk.ng@gmail.com>
2020-02-06 10:18:30 -0800

    what to do, what to do?

--- todo.js
+++ todo.js
@@ -1,17 +1,53 @@
-/*
-What types of objects do you need?
+const fs = require('fs');
 
-List out nouns and verbs involved in creating TODO lists.
-Decide which nouns and verbs you want/need to model.
-The nouns will be your objects and the values the functions.
+let currentList = fs.readFileSync('todos.txt', 'utf-8');
+let command = process.argv[2];
+let input = process.argv[3];
 
-Write simple functions that work on a few number of well-defined objects.
+function seeList() {
+  if (command === 'list') {
+    console.log(fs.readFileSync('todos.txt', 'utf-8'));
+  }
+}
 
-Keep the responsibilities separated as best you can:
+function addTo() {
+  if (command === 'add') {
+    console.log('Adding "' + input + '" to TODO list.');
+    fs.writeFileSync('todos.txt', currentList + '[ ] ' + input + '\n');
+  }
+}
 
-1. Representing a real-life todo list as in-memory objects
-2. Manipulating those in-memory objects
-3. Reading and writing from the todos.txt file
-4. Displaying information to the user
-5. Rather user input and taking the appropriate actions
-*/
+function takeAway() {
+  if (command === 'delete') {
+    let listArray = currentList.split('\n');
+    let place = listArray[input - 1];
+    let item = place.slice(4, place.length);
+    console.log('Removing "' + item + '" from TODO list.');
+    let lines = currentList.split('\n');
+    lines.splice(input - 1, 1);
+    let newList = lines.join('\n');
+    fs.writeFileSync('todos.txt', newList);
+  }
+}
+
+function checkmark() {
+  if (command === 'complete') {
+    let listArray = currentList.split('\n');
+    let place = listArray[input - 1];
+    let item = place.slice(4, place.length);
+    console.log('Marking "' + item + '" as complete!');
+    let markedBox = place.replace('[ ]', '[X]');
+    let lines = currentList.split('\n');
+    let lines2 = currentList.split('\n');
+    let sectOne = lines.splice(0, input - 1);
+    let sectTwo = lines2.splice(input, lines.length);
+    let space = (sectOne + ',' + markedBox + ',' + sectTwo);
+    let newList = space.replace(/,/g, '\n');
+    fs.writeFileSync('todos.txt', newList);
+  }
+}
+
+seeList();
+addTo();
+takeAway();
+checkmark();
